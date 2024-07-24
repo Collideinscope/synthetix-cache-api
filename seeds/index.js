2d0948688ed891b43b0532e63ffdb699b7a5abca
collideinscope <collide.in.scope4@gmail.com>
2024-07-24 07:38:54 +0400

    updated

--- seeds/index.js
+++ seeds/index.js
@@ -7,14 +7,14 @@ process.env.TZ = 'UTC';
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  /*
+  
   await knex('apy').del();
   await knex('tvl').del();
   await knex('core_delegations').del();
   await knex('pool_rewards').del();
   await knex('core_account_delegations').del();
   await knex('perp_stats').del();
-  await knex('perp_account_stats').del(); */
+  await knex('perp_account_stats').del(); 
   await knex('perp_market_history').del();
 
   await seedAllData();
